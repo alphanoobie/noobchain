d93d96ffdee0f09432943dc265404ee299b10910
alphanoobie <hussaingadi.hg@gmail.com>
2023-02-20 20:00:11 +0530

    blockchain class skeleton

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block():
+        pass
+
+    def new_transaction():
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    @property
+    def last_block(self):
+        pass
