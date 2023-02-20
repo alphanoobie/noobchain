99ae74df29ffd2ffe4d4d94ea8015a36266db7b6
alphanoobie <hussaingadi.hg@gmail.com>
2023-02-20 20:24:44 +0530

    new block, new txn, hash

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,42 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block():
-        pass
+        #Genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction():
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index' : len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions' : self.current_transactions,
+            'proof' : proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+
+    def new_transaction(self, sender, receipient, amount):
+        self.current_transactions.append({
+            'sender':sender,
+            'receipient':receipient,
+            'amount':amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
